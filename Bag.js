583bb4d1e22ed6477abc81aa0ea6b3f1c8130087
rbenitte <benitteraphael@gmail.com>
2012-06-12 11:46:33 +0200

    Initial commit

---
+++ Bag.js
@@ -0,0 +1,165 @@
+/**
+ * Bag class.
+ * Lightweight wrapper for javascript config objects.
+ *
+ * @author Raphaël Benitte <benitterapheal@gmail.com>
+ * @since  2012-11-06
+ */
+var Bag = function() {
+    this.fields = {};
+};
+Bag.prototype = {
+
+    seto: function(options) {
+        var processedOptions = {};
+        if (options.hasOwnProperty('fields')) {
+
+            // options must be an object
+            if ({}.toString.call(options) !== '[object Object]') {
+                throw 'Field fields must be an object';
+            }
+
+            processedOptions.fields = {};
+            for (var fieldName in options.fields) {
+                if (options.fields.hasOwnProperty(fieldName)) {
+                    processedOptions.fields[fieldName] = this.seto(options.fields[fieldName]);
+                }
+            }
+        } else {
+            processedOptions.required = !!(options.hasOwnProperty('required') && options.required === true);
+            processedOptions.defaultValue = options.hasOwnProperty('defaultValue') ? options.defaultValue : null;
+        }
+
+        return processedOptions;
+    },
+
+    /**
+     * Add a field with options.
+     *
+     * @param fieldName the field name
+     * @param options   an object with field config
+     * @return Bag
+     */
+    add: function(fieldName, options) {
+
+        // field name must be a valid property name
+        if ({}.toString.call(fieldName) !== '[object String]') {
+            throw 'Invalid field name, field name must be a string';
+        }
+
+        if ({}.toString.call(options) === '[object Object]') {
+            this.fields[fieldName] = this.seto(options);
+        } else {
+            throw 'Invalid options, options must be an Object';
+        }
+
+        return this;
+    },
+
+    /**
+     * Repeat a String n times.
+     *
+     * @param str   string to repeat
+     * @param count repeat count
+     * @return String
+     */
+    repeatString: function(str, count) {
+        var repeated = "";
+        for (var a = 0; a < count; a++) {
+            repeated += str;
+        }
+        return repeated;
+    },
+
+    /**
+     * Returns a string representation of the Bag.
+     *
+     * @param fields [optional] an object with field config
+     * @param depth  [optional] an integer representing current field depth in the Bag
+     * @return String
+     */
+    toString: function(fields, depth) {
+
+        fields = fields || this.fields;
+        depth  = depth  || 0;
+
+        var fieldsString = '';
+
+        for (var fieldName in fields) {
+            if (fields.hasOwnProperty(fieldName)) {
+                var fieldOptions = fields[fieldName];
+                if (fieldOptions.hasOwnProperty('fields')) {
+                    fieldsString += this.repeatString(" ", depth * 2) + fieldName + ": {\n";
+                    fieldsString += this.toString(fieldOptions.fields, depth + 1);
+                    fieldsString += this.repeatString(" ", depth * 2) + "}\n";
+                } else {
+                    fieldsString += this.repeatString(" ", depth * 2) + fieldName + ": { ";
+                    fieldsString += "required: " + fieldOptions.required + ', ';
+                    fieldsString += "defaultValue: " + fieldOptions.defaultValue + " }\n";
+                }
+            }
+        }
+
+        return fieldsString;
+    },
+
+    isObjectRequired: function(fields) {
+
+        for (var fieldName in fields) {
+            if (fields.hasOwnProperty(fieldName)) {
+                var fieldConfig = fields[fieldName];
+                if (fieldConfig.hasOwnProperty('required') && fieldConfig.required === true) {
+                    return fieldName;
+                } else if (fieldConfig.hasOwnProperty('fields')) {
+                    var isRequired = this.isObjectRequired(fieldConfig.fields);
+                    if (isRequired !== false) {
+                        return isRequired;
+                    }
+                }
+            }
+        }
+
+        return false;
+    },
+
+    put: function(config, options) {
+
+        if ({}.toString.call(config) !== '[object Object]') {
+            throw 'Invalid config, config must be an Object';
+        }
+
+        options = options || this.fields;
+        for (var fieldName in options) {
+
+            if (options.hasOwnProperty(fieldName)) {
+
+                var fieldOptions = options[fieldName];
+
+                // for object fields
+                if (fieldOptions.hasOwnProperty('fields')) {
+                    if (!config.hasOwnProperty(fieldName)) {
+                        var isRequired = this.isObjectRequired(fieldOptions.fields);
+                        if (isRequired !== false) {
+                            throw 'Encountred required sub field "' + isRequired + '" for non existing field "' + fieldName + '"';
+                        }
+                    } else {
+                        config[fieldName] = this.put(config[fieldName], fieldOptions.fields);
+                    }
+
+                // for flat field
+                } else {
+                    if (fieldOptions.required === true
+                     && (config.hasOwnProperty(fieldName) || config[fieldName] === null || config[fieldName] === undefined)) {
+                        if (fieldOptions.defaultValue !== null && fieldOptions.defaultValue !== undefined) {
+                            config[fieldName] = fieldOptions.defaultValue;
+                        } else {
+                            throw 'Field "' + fieldName + '" is required and does not have a default value';
+                        }
+                    }
+                }
+            }
+        }
+
+        return config;
+    }
+};
---
+++ test/spec/BagSpec.js
@@ -0,0 +1,119 @@
+describe('Bag', function() {
+
+    it('should throw exception if trying to add a field with an invalid field name', function() {
+        var bag = new Bag();
+        expect(function(){
+            bag.add(1, {});
+        }).toThrow('Invalid field name, field name must be a string');
+    });
+
+    it('should throw exception if trying to add a field with invalid options', function() {
+        var bag = new Bag();
+        expect(function(){
+            bag.add('name', 1);
+        }).toThrow('Invalid options, options must be an Object');
+    });
+
+    it('should have a required field', function() {
+        var bag = new Bag();
+        bag.add('name', {
+            required: true
+        });
+        expect(bag.fields.name.required).toBe(true);
+    });
+
+    it('should have a required field with default value', function() {
+        var bag = new Bag();
+        bag.add('name', {
+            required: true,
+            defaultValue: 'John'
+        });
+        expect(bag.fields.name.required).toBe(true);
+        expect(bag.fields.name.defaultValue).toBe('John');
+    });
+
+    it('should use default value if field is null', function() {
+        var bag = new Bag();
+        bag.add('name', {
+            required: true,
+            defaultValue: 'John'
+        });
+        expect(bag.fields.name.required).toBe(true);
+        expect(bag.fields.name.defaultValue).toBe('John');
+        var processed = bag.put({
+            name: null
+        });
+        expect(processed.name).toBe('John');
+    });
+
+    it('should support nested fields', function() {
+        var bag = new Bag();
+        bag.add('address', {
+            fields: {
+                number:  { required: true },
+                street:  { fields: {
+                    type: {
+                        required: true,
+                        defaultValue: 'street'
+                    },
+                    name: {
+                        required: true
+                    }
+                }}
+            }
+        });
+        expect(bag.fields.address).toBeDefined();
+        expect(bag.fields.address.fields).toBeDefined();
+        expect(bag.fields.address.fields.number).toBeDefined();
+        expect(bag.fields.address.fields.number.required).toBe(true);
+        expect(bag.fields.address.fields.street).toBeDefined();
+
+        expect(bag.fields.address.fields.street.fields).toBeDefined();
+        expect(bag.fields.address.fields.street.fields.type).toBeDefined();
+        expect(bag.fields.address.fields.street.fields.type.required).toBe(true);
+        expect(bag.fields.address.fields.street.fields.type.defaultValue).toBe('street');
+        expect(bag.fields.address.fields.street.fields.name).toBeDefined();
+        expect(bag.fields.address.fields.street.fields.name.required).toBe(true);
+    });
+
+    it('should throw exception if an object field is not set and has required sub fields', function() {
+        var bag = new Bag();
+        bag.add('address', {
+            fields: {
+                number:  { required: false },
+                street:  { fields: {
+                    type: {
+                        required: true,
+                        defaultValue: 'street'
+                    },
+                    name: {
+                        required: true
+                    }
+                }}
+            }
+        });
+        expect(function(){
+            bag.put({});
+        }).toThrow('Encountred required sub field "type" for non existing field "address"');
+    });
+
+    it('should output a string representation of the bag config for non nested fields', function() {
+        var bag = new Bag();
+        bag.add('firstname', { required: true })
+           .add('lastname',  { required: true });
+        expect(bag.toString()).toBe("firstname: { required: true, defaultValue: null }\nlastname: { required: true, defaultValue: null }\n");
+    });
+
+    it('should output a string representation of the bag config for nested fields', function() {
+        var bag = new Bag();
+        bag.add('firstname', { required: true })
+           .add('lastname',  { required: true })
+           .add('address', {
+               fields: {
+                   number: { required: true },
+                   street: { required: true }
+               }
+           });
+        expect(bag.toString()).toBe("firstname: { required: true, defaultValue: null }\nlastname: { required: true, defaultValue: null }\naddress: {\n  number: { required: true, defaultValue: null }\n  street: { required: true, defaultValue: null }\n}\n");
+    });
+});
